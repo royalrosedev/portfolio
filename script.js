8fc29329dcc6657f4cb911defeeea8b995f4391e
RoyalRose <mensior.kids@gmail.com>
2025-04-22 05:15:39 -0600

    woahhh, portfolio

---
+++ script.js
@@ -0,0 +1,37 @@
+const HEADER = `
+<header>
+    <h1>Ethan Mensior</h1>
+    <nav>
+        <a href="#about"><button>About Me</button></a>
+        <a href="#projects"><button>Projects</button></a>
+        <a href=""><button>Certifications</button></a>
+        <a href=""><button>Contact Me!</button></a>
+        <a href="transcript.html"><button>Resume + Transcript</button></a>
+    </nav>
+
+</header>
+`
+
+const FOOTER = `
+<footer>
+    <p>&copy 2025 Ethan Mensior, Meridian Technical Charter High school</p>
+
+</footer>
+`
+// add a github button and a linked in button
+
+// <i style="font-size:2rem" class="fa">&#xf08c;</i>
+// <a href="https://www.linkedin.com/in/ethan-mensior/"><i style="font-size:2rem" class="fa">&#xf09b;</i></a>
+
+
+window.onload = function() {
+   let header = document.createElement('div')
+   document.querySelector('body').prepend(header)
+   header.outerHTML = HEADER
+
+   let footer = document.createElement('div')
+   document.querySelector('body').append(footer)
+   footer.outerHTML = FOOTER
+}
+
+
